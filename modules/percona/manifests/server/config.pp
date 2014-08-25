38d3fe87322fcd06f79eef29cf8b113849564048
lamanotrama <lamanotrama@gmail.com>
2014-08-26 02:42:31 +0900

    remove. perconaモジュール使わないようにしたので削除

--- modules/percona/manifests/server/config.pp
+++
@@ -1,9 +0,0 @@
-class percona::server::config {
-
-  file { '/etc/my.cnf':
-    ensure  => present,
-    content => template('percona/server/my.cnf.erb'),
-    notify  => Service['mysql'],
-  }
-
-}
