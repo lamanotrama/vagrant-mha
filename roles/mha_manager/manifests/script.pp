a941af67e387daf0624dcca8e46678c574b748c2
lamanotrama <lamanotrama@gmail.com>
2014-09-07 23:12:21 +0900

    mha_manager roleにスクリプトソース管理用のmanifestを追加

---
+++ roles/mha_manager/manifests/script.pp
@@ -0,0 +1,18 @@
+class mha_manager::script {
+
+  file { '/usr/local/libexec':
+    ensure => dirctory;
+  }
+
+  file {
+    '/usr/local/libexec/master_ip_failover':
+      source => 'puppet://modules/mha_manager/usr/local/libexec/master_ip_failover',
+      owner  => 'root',
+      mode   => 700;
+
+    '/usr/local/libexec/master_ip_online_change':
+      source => 'puppet://modules/mha_manager/usr/local/libexec/master_ip_online_change',
+      owner  => 'root',
+      mode   => 700;
+  }
+}
