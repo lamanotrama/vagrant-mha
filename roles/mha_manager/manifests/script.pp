dfcb975671b42906f324236c4f7fd7e79ce4bf0f
lamanotrama <lamanotrama@gmail.com>
2014-09-08 21:58:04 +0900

    フェイルオーバ確認用に簡単なスクリプトをmanagerホストに追加

--- roles/mha_manager/manifests/script.pp
+++ roles/mha_manager/manifests/script.pp
@@ -11,4 +11,16 @@ class mha_manager::script {
       owner  => 'root',
       mode   => 700;
   }
+
+  $vip  = hiera('mha::vip')
+  $user = hiera('mha::user')
+  $pass = hiera('mha::password')
+
+  file {
+    '/usr/local/bin/mysql_connect_test':
+      content => "#!/bin/sh\n\nwhile :; do mysql -BN --connect-timeout=1 -h ${vip} -u ${user} -p${pass} -e 'select sysdate(),@@server_id'; sleep 1; done\n",
+      owner   => 'root',
+      mode    => 700;
+  }
+
 }
